bc61fdb7fdf4cd9f451e26ae90abd6ed7c0e0b36
its-acarn <andrew.carnaghan@walksafe.io>
2023-12-19 12:13:10 +0000

    Chnages from tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { ErrorComponent, ThemedLayoutV2, notificationProvider, RefineThemes } from "@refinedev/chakra-ui";
-import { Refine } from "@refinedev/core";
-import routerBindings, { NavigateToResource, UnsavedChangesNotifier } from "@refinedev/react-router-v6";
+import { ErrorComponent, ThemedLayoutV2, notificationProvider, RefineThemes, AuthPage } from "@refinedev/chakra-ui";
+import { Authenticated, Refine } from "@refinedev/core";
+import routerBindings, { CatchAllNavigate, NavigateToResource, UnsavedChangesNotifier } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { ChakraUIInferencer } from "@refinedev/inferencer/chakra-ui";
+
+import { BlogPostList } from "./pages/blog-posts/list";
+import { BlogPostEdit } from "./pages/blog-posts/edit";
+import { BlogPostShow } from "./pages/blog-posts/show";
 
 const App = () => {
   return (
@@ -21,6 +25,9 @@ const App = () => {
               show: "/blog-posts/show/:id",
               create: "/blog-posts/create",
               edit: "/blog-posts/edit/:id",
+              meta: {
+                canDelete: true,
+              },
             },
           ]}
           options={{
@@ -31,18 +38,41 @@ const App = () => {
           <Routes>
             <Route
               element={
-                <ThemedLayoutV2>
-                  <Outlet />
-                </ThemedLayoutV2>
+                <Authenticated fallback={<CatchAllNavigate to="/login" />}>
+                  <ThemedLayoutV2>
+                    <Outlet />
+                  </ThemedLayoutV2>
+                 </Authenticated>
               }
             >
               <Route index element={<NavigateToResource resource="blog_posts" />} />
               <Route path="blog-posts">
-                <Route index element={<ChakraUIInferencer />} />
-                <Route path="show/:id" element={<ChakraUIInferencer />} />
-                <Route path="edit/:id" element={<ChakraUIInferencer />} />
+                <Route index element={<BlogPostList />} />
+                <Route path="show/:id" element={<BlogPostShow />} />
+                <Route path="edit/:id" element={<BlogPostEdit />} />
                 <Route path="create" element={<ChakraUIInferencer />} />
               </Route>
+            </Route>
+
+            <Route
+              element={
+                <Authenticated fallback={<Outlet />}>
+                  <NavigateToResource />
+                </Authenticated>
+              }
+            >
+              <Route path="/login" element={<AuthPage type="login" />} />
+            </Route>
+            
+            <Route
+              element={
+                <Authenticated fallback={<Outlet />}>
+                  <ThemedLayoutV2>
+                    <Outlet />
+                  </ThemedLayoutV2>
+                </Authenticated>
+              }
+            >
               <Route path="*" element={<ErrorComponent />} />
             </Route>
           </Routes>
